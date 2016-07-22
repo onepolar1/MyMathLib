3f8a7c410f408814694589b29b6c969a1ee007fb
iefan <mybsppp@163.com>
2016-07-22 11:33:03 +0800

    测试使文本框中的图片变小

--- ex11_textInsertImage.py
+++ ex11_textInsertImage.py
@@ -1,36 +1,113 @@
-import sys
-from PyQt4 import QtCore, QtGui
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
 
-class Example(QtGui.QWidget):
+from resources import *
 
-    def __init__(self):
-        super(Example, self).__init__()
+class MyWindow(QWidget):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
 
-        self.initUI()
+        self.pushButtonImage = QPushButton(self)
+        self.btn2 = QPushButton(self)
+        self.pushButtonImage.setText("Insert Image!")
+        self.btn2.setText("测试")
+        self.pushButtonImage.clicked.connect(self.on_pushButtonImage_clicked)
+        self.btn2.clicked.connect(self.resizeImage)
 
-    def initUI(self):
+        self.textEditImage = QTextEdit(self)
+        self.textEditImage.setPlainText("Insert an image here:")
 
-        textEdit = QtGui.QTextEdit('',self)
-        textEdit.setGeometry(QtCore.QRect(300, 300, 640, 480))
-        textEdit.move(0, 0)
-        self.setGeometry(300, 300, 640, 480)
+        self.layoutVertical = QVBoxLayout(self)
+        self.layoutVertical.addWidget(self.pushButtonImage)
+        self.layoutVertical.addWidget(self.btn2)
+        self.layoutVertical.addWidget(self.textEditImage)
 
-        img = QImage('images/trash.png','PNG')
+        self.resizeImage()
 
-        cursor = QTextCursor(textEdit.document())
-        cursor.insertText("Hello World")
-        cursor.insertImage(img)
+    def resizeImage(self):
+        currentBlock = self.textEditImage.textCursor().block()
+        it = QTextBlock.iterator()
+        it = currentBlock.begin()
+        while it != currentBlock.end():            
+            currentFragment = it.fragment()
+            it += 1
+            # print(it)
+            
+            if currentFragment.isValid():
+                if currentFragment.charFormat().isImageFormat ():
+                    newImageFormat = currentFragment.charFormat().toImageFormat()
+                    size = [newImageFormat.width(), newImageFormat.height()]
+                    # print(size)
 
-        self.show()
+                    newImageFormat.setWidth(size[0]*0.8)
+                    newImageFormat.setHeight(size[1]*0.8)
 
-def main():
+                    if  newImageFormat.isValid():
+                        #QMessageBox::about(this, "Fragment", currentFragment.text());
+                        #newImageFormat.setName(":/icons/text_bold.png");
+                        helper = self.textEditImage.textCursor()
 
-    app = QtGui.QApplication(sys.argv)
-    ex = Example()
+                        helper.setPosition(currentFragment.position());
+                        helper.setPosition(currentFragment.position() + currentFragment.length(), QTextCursor.KeepAnchor);
+                        helper.setCharFormat(newImageFormat)
+                      
+                     
+             
+        # print(self.textEditImage.textCursor().block())
+
+
+        # QTextBlock currentBlock = m_textEdit->textCursor().block();
+
+
+
+    def on_pushButtonImage_clicked(self):
+        filePath = QFileDialog.getOpenFileName(
+            self,
+            "Select an image",
+            ".",
+            "Image Files(*.png *.gif *.jpg *jpeg *.bmp)"
+        )
+
+        # print(filePath)
+        if filePath:
+            self.insertImage(filePath)
+
+    def insertImage(self, filePath):
+        imageUri = QUrl("file://{0}".format(filePath))
+        # print(imageUri, "===")
+        image    = QImage(QImageReader(filePath).read())
+
+        self.textEditImage.document().addResource(
+            QTextDocument.ImageResource,
+            imageUri,
+            image
+        )
+
+        imageFormat = QTextImageFormat()
+        imageFormat.setWidth(100)
+        # imageFormat.setWidth(image.width())
+        imageFormat.setHeight(100)
+        # imageFormat.setHeight(image.height())
+        imageFormat.setName(imageUri.toString())
+
+        textCursor = self.textEditImage.textCursor()
+        textCursor.movePosition(
+            QTextCursor.End,
+            QTextCursor.MoveAnchor
+        )
+        textCursor.insertImage(imageFormat)
+
+        # This will hide the cursor
+        blankCursor = QCursor(Qt.BlankCursor)
+        self.textEditImage.setCursor(blankCursor)
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    app.setApplicationName('MyWindow')
+
+    main = MyWindow()
+    main.show()
+
     sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
